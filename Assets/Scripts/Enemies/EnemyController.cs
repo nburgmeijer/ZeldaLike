03e76553889b4a0735a5c689dff1299891d5cb30
nburgmeijer <nick.burgmeijer@nhlstenden.com>
2020-05-10 20:58:39 +0200

    enemy fsm progress

---
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private IEnemyState _currentState;
+    private Animator _animator;
+    private Rigidbody2D _rigidBody2D;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Enemies/IEnemyState.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IEnemyState
+{
+    void EnterState(EnemyController enemy);
+    void Update(EnemyController enemy);
+    void OnCollisionEnter2D(EnemyController enemy);
+}
